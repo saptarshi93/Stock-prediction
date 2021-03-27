e4c13bc47d151574136deca256d067f4bb0fbe34
saptarshi93 <saptarshi.mukherjee4@gmail.com>
2021-03-27 11:19:19 +0100

    Add files via upload

---
+++ company_keywords.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 27 11:10:16 2021
+
+@author: Acer
+"""
+
+key_words ={'Apple': ['Apple', 'IPod', 'MacOS', 'Mac App Store', 'IWork', 'IOS', 'IPad', 'App Store', 'Macintosh', 'Apple Store', 'ILife', 'TvOS', 'Apple Music', 'ICloud', 'Apple TV', 'IPhone', 'Apple Store', 'ITunes Store', 'Apple Watch', 'Apple Pay', 'WatchOS', 'IBooks Store', 'Arthur D. Levinson', 'Steve Wozniak', 'Steve Jobs', 'Jonathan Ive', 'Ronald Wayne', 'Jeff Williams', 'Luca Maestri', 'Tim Cook', 'FileMaker', 'Anobit', 'Beats Electronics', 'Braeburn Capital'],\
+            'Amgen': ['Mimpara', 'Aranesp', 'Sensipar', 'Enbrel', 'Vectibix', 'Neulasta', 'Neupogen', 'Anakinra', 'Epogen', 'Prolia', 'Nplate', 'Xgeva', 'Amgen', 'biotechnology', ' Applied Molecular Genetics', ' Molecular Genetics', 'Robert A. Bradway'],
+            'Google': ['GV', 'Sidewalk Labs', 'Vevo', 'Google Fiber', 'Stackdriver', 'VirusTotal', 'Calico', 'Google', 'Jigsaw', 'Nest Labs', 'Firebase', 'Google Capital', 'Verily', 'GV', 'Sidewalk Labs', 'Vevo', 'Google Fiber', 'Stackdriver', 'VirusTotal', 'Calico', 'Alphabet'],
+            'Microsoft': ['Microsoft Office', 'Microsoft Edge', 'Microsoft Windows', 'Microsoft Surface', 'Xbox', 'Microsoft TechNet', 'OneDrive', 'Microsoft Visual Studio', 'Outlook', 'Microsoft Developer Network', 'Microsoft Azure', 'Microsoft Dynamics', 'MSN', 'Skype', 'Microsoft Servers', 'Xbox Live', 'Bing', 'Windows Operating system', 'Microsoft Mobile', 'Bill Gates', 'Paul Allen', 'John W. Thompson', 'Satya Nadella', 'Cloud computing', 'DotNet'], 
+            'Netflix': ['Reed Hastings', 'Ted Sarandos', 'Greg Peters', 'online series', 'Netflix', 'Streaming'],
+            'Facebook': ['Facebook', 'Mark Zuckerberg', 'The Facebook', 'WhatsApp', 'Messenger', 'Social media'],
+            'Amazon': ['Amazon Video', 'Kindle Fire', 'Twitch.tv', 'Amazon Kindle', 'Lab126', 'ComiXology', 'Amazon Studios', 'Amazon Web Services', 'Online shopping', 'Web hosting', 'Woot', 'Askville', 'Amazon Appstore', 'Audible', 'Amazon Game Studios', 'Digital distribution', 'Amazon Instant Video UK', 'Amazon Echo', 'The Book Depository', 'Amazon Instant Video German', 'Jeff Bezos', 'Werner Vogels', 'Goodreads', 'Zappos', 'ComiXology', 'Alexa Internet', 'Amazon Web Services', 'Internet Movie Database', 'A9', 'Audible', 'Junglee', 'Digital Photography Review', 'Amazon', 'cloud computing', 'Twitch'],
+            'AAL':['Aviaition','American Airlines']
+            
+    
+}
